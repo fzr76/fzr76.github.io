7cfe89167113e6b0496145d8e2b0901c1d036f42
fzr76 <gaitheraaronjosh@protonmail.com>
2023-07-15 07:40:08 -0400

    Half fixed animation delay when resizing, still a WIP

--- Resources/JS/loadScreen.js
+++ Resources/JS/loadScreen.js
@@ -4,6 +4,18 @@ window.addEventListener('DOMContentLoaded', function() {
       // fade out
       overlay.style.opacity = 0;
       // remove after transition
-      setTimeout(function(){ overlay.style.display = "none"; }, 500); // 500ms is the transition time
+      setTimeout(function(){ 
+        overlay.style.display = "none";
+        // Add class 'overlay-gone' to body when overlay is not displayed
+        document.body.classList.add('overlay-gone');
+      }, 500); // 500ms is the transition time
     }, 4000); // 4000ms = 4 seconds
+
+    window.addEventListener('resize', function() {
+        if(overlay.style.display === "none") {
+            document.body.classList.add('overlay-gone');
+        } else {
+            document.body.classList.remove('overlay-gone');
+        }
+    });
 });
