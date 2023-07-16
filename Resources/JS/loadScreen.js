03091815d31238ed33b51f7382557cc044818a19
fzr76 <gaitheraaronjosh@protonmail.com>
2023-07-16 14:13:25 -0400

    Finally fixed all animations with my overlay. All visitors will see things properly!

--- Resources/JS/loadScreen.js
+++ Resources/JS/loadScreen.js
@@ -5,17 +5,16 @@ window.addEventListener('DOMContentLoaded', function() {
       overlay.style.opacity = 0;
       // remove after transition
       setTimeout(function(){ 
-        overlay.style.display = "none";
-        // Add class 'overlay-gone' to body when overlay is not displayed
-        document.body.classList.add('overlay-gone');
+          overlay.style.display = "none";
+          // remove initial-animation class after the overlay is gone
+          removeInitialAnimationClass();
       }, 500); // 500ms is the transition time
     }, 4000); // 4000ms = 4 seconds
-
-    window.addEventListener('resize', function() {
-        if(overlay.style.display === "none") {
-            document.body.classList.add('overlay-gone');
-        } else {
-            document.body.classList.remove('overlay-gone');
-        }
-    });
 });
+
+function removeInitialAnimationClass() {
+    var elements = document.querySelectorAll('.initial-animation');
+    elements.forEach(function(element) {
+        element.classList.remove('initial-animation');
+    });
+}
