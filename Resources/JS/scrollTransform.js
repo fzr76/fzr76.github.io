1bb4a5c30bb1999f17bf0a133acb7d557a86bcae
fzr76 <gaitheraaronjosh@protonmail.com>
2023-07-18 07:59:20 -0400

    Added some scroll affect to the desktop site. WIP

--- Resources/JS/scrollTransform.js
+++ Resources/JS/scrollTransform.js
@@ -1,11 +1,21 @@
 window.addEventListener('scroll', function() {
-        if (window.scrollY > 0) {
-            document.body.classList.add('scroll');
-        } else {
-            document.body.classList.remove('scroll');
-        }
-});
+    var diagContentLinks = document.querySelectorAll(".diag-content a");
 
-window.addEventListener('scroll', function() {
-    console.log(window.scrollY);
+    if (window.scrollY > 0) {
+        document.body.classList.add('scroll');
+        document.querySelector(".diagonal-column1").classList.add("unskew");
+        document.querySelector(".diagonal-column2").classList.add("unskew");
+        document.querySelector(".sidebar .diag-content").classList.add("unskew");
+        for (var i = 0; i < diagContentLinks.length; i++) {
+            diagContentLinks[i].classList.add("unskew");
+        }
+    } else {
+        document.body.classList.remove('scroll');
+        document.querySelector(".diagonal-column1").classList.remove("unskew");
+        document.querySelector(".diagonal-column2").classList.remove("unskew");
+        document.querySelector(".sidebar .diag-content").classList.remove("unskew");
+        for (var i = 0; i < diagContentLinks.length; i++) {
+            diagContentLinks[i].classList.remove("unskew");
+        }
+    }
 });
