1a9c909ce4365732499ab2cd4b8331e55b7e82e6
fzr76 <112741737+fzr76@users.noreply.github.com>
2023-07-20 12:33:03 -0400

    Fix attempt for mobile hamburger menu

--- Resources/JS/hamburgerMenu.js
+++ Resources/JS/hamburgerMenu.js
@@ -1,18 +1,32 @@
-// Get the sidebar and hamburger checkbox elements
-const sidebar = document.querySelector('.sidebar');
-const checkbox = document.querySelector('#hamburger-checkbox');
-
-  // Add a click event listener to each link in the sidebar
-const links = sidebar.querySelectorAll('a');
-links.forEach(link => {
-    link.addEventListener('click', () => {
-      // Uncheck the hamburger checkbox to close the sidebar
-        checkbox.checked = false;
-    });
-});
-
 $(document).ready(function() {
   $('#hamburger-checkbox').change(function() {
       $('.hamburger-menu, .sidebar').toggleClass('active', this.checked);
+      if (!this.checked) {
+          // If the checkbox is not checked (menu is closed), uncheck all checkboxes
+          const sidebar = document.querySelector('.sidebar');
+          const links = sidebar.querySelectorAll('a');
+          links.forEach(link => {
+              link.click();
+          });
+      }
   });
 });
+
+// Get the sidebar and hamburger checkbox elements
+// const sidebar = document.querySelector('.sidebar');
+// const checkbox = document.querySelector('#hamburger-checkbox');
+
+// Add a click event listener to each link in the sidebar
+// const links = sidebar.querySelectorAll('a');
+// links.forEach(link => {
+//     link.addEventListener('click', () => {
+//       // Uncheck the hamburger checkbox to close the sidebar
+//         checkbox.checked = false;
+//     });
+// });
+
+// $(document).ready(function() {
+//   $('#hamburger-checkbox').change(function() {
+//       $('.hamburger-menu, .sidebar').toggleClass('active', this.checked);
+//   });
+// });
