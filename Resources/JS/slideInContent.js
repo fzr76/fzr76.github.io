e8733e306b84514075d222b00341f98efe1064cb
fzr76 <gaitheraaronjosh@protonmail.com>
2023-07-21 05:35:49 -0400

    Fixed top scroll button and added slide in animation to content of the page

---
+++ Resources/JS/slideInContent.js
@@ -0,0 +1,14 @@
+// This code will have content slide in to the screen when you scroll past it
+window.addEventListener('scroll', function() {
+    const content = document.getElementById('Content');
+    const elements = content.children;
+
+    Array.from(elements).forEach(element => {
+        const rect = element.getBoundingClientRect();
+        const top = rect.top;
+
+        if(top <= window.innerHeight / 2) {
+            element.classList.add('animate');
+        }
+    });
+});
