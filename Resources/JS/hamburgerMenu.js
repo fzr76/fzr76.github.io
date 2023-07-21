5ac3fc43525939b2c77502ba949ede83886ef4e4
fzr76 <gaitheraaronjosh@protonmail.com>
2023-07-20 21:27:48 -0400

    Added some styling to top page button

--- Resources/JS/hamburgerMenu.js
+++ Resources/JS/hamburgerMenu.js
@@ -10,23 +10,3 @@ $(document).ready(function() {
         $('.hamburger-menu, .sidebar').removeClass('active');
     });
 });
-
-
-// Get the sidebar and hamburger checkbox elements
-// const sidebar = document.querySelector('.sidebar');
-// const checkbox = document.querySelector('#hamburger-checkbox');
-
-// Add a click event listener to each link in the sidebar
-// const links = sidebar.querySelectorAll('a');
-// links.forEach(link => {
-//     link.addEventListener('click', () => {
-//       // Uncheck the hamburger checkbox to close the sidebar
-//         checkbox.checked = false;
-//     });
-// });
-
-// $(document).ready(function() {
-//   $('#hamburger-checkbox').change(function() {
-//       $('.hamburger-menu, .sidebar').toggleClass('active', this.checked);
-//   });
-// });
