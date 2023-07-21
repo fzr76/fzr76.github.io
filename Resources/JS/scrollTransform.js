b3d9a3e80dce52dcc534c0913ed300e06d5d7aca
fzr76 <gaitheraaronjosh@protonmail.com>
2023-07-21 09:26:28 -0400

    Made hamburger menu easier to see and added top blur bar to mobile for easier viewing

--- Resources/JS/scrollTransform.js
+++ Resources/JS/scrollTransform.js
@@ -2,19 +2,22 @@ window.addEventListener('scroll', function() {
     const navbar = document.getElementById('myNavbar');
     const diagonalColumn = document.getElementById('diagonal-column2');
     const topButton = document.getElementById('top');
+    const mobileBar = document.getElementById('mobileTopBlur')
     
     if (window.scrollY > 500) {
         document.body.classList.add('scroll');
         navbar.classList.add('show');
         diagonalColumn.style.display = "none";
         topButton.style.display = "flex";
-        diagonalColumn.classList.remove('slide-in');  // remove the animation class
+        diagonalColumn.classList.remove('slide-in');
+        mobileBar.classList.add('show');
     } else {
         document.body.classList.remove('scroll');
         navbar.classList.remove('show');
         diagonalColumn.style.display = "block";
         topButton.style.display = "none";
-        void diagonalColumn.offsetWidth;  // trigger a reflow (flush of the CSS cache)
-        diagonalColumn.classList.add('slide-in');  // re-add the animation class
+        void diagonalColumn.offsetWidth;
+        diagonalColumn.classList.add('slide-in');
+        mobileBar.classList.remove('show')
     }
 });
