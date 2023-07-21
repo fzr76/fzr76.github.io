405009a5200afa145f0accb3168a513cc45cd396
fzr76 <gaitheraaronjosh@protonmail.com>
2023-07-21 06:40:27 -0400

    Another attempt to fix contact form

--- Resources/PHP/process_form.php
+++ Resources/PHP/process_form.php
@@ -1,20 +1,23 @@
+<?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Fetch form data
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
-    
-    // Set your email address where you want to receive inquiries
+
+    // Set your email address where you want to receive the form submissions
     $to = "gaitheraaronjosh@protonmail.com";
-    
-    // Email subject and content
-    $subject = "Someone Reached Out From Your Website!";
-    $content = "Name: $name\nEmail: $email\nMessage: $message";
-    
-    // Send email
-    if (mail($to, $subject, $content)) {
-        echo "Thank you for contacting me!";
-    } else {
-        echo "Oops! Something went wrong. Please try again later.";
-    }
+    $subject = "New Contact Form Submission";
+
+    // Compose the email body
+    $email_body = "Name: $name\n";
+    $email_body .= "Email: $email\n";
+    $email_body .= "Message:\n$message";
+
+    // Send the email
+    mail($to, $subject, $email_body);
+
+    // Redirect the user back to the contact form after submission
+    header("Location: index.html#footer");
+    exit;
 }
+?>
